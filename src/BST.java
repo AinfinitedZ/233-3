de516d8a0277db06a0c811ba239120c3fa2533f0
AinfinitedZ_Surface <daniel_sheep@163.com>
2022-02-20 09:33:04 -0500

    first commit

---
+++ src/BST.java
@@ -0,0 +1,7 @@
+public class BST<T,V> implements tree<T,V>{
+    private class BN{
+        private T key;
+        private V value;
+        
+    }
+}
---
+++ src/tree.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface tree <T,V> {
+    void insert(T key, V value);
+    V search(T key);
+    void delete(T key);
+    List<V> inorderRec();
+    V kthSmallest(int k);
+}
+
+
